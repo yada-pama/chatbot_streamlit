7db440ad928c64bd7dbd788ec87b0923d1a6e0bc
yada-pama <156170411+yada-pama@users.noreply.github.com>
2024-11-27 13:08:55 +0700

    Update typhoon streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,10 @@ if not openai_api_key:
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    #client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(base_url='https://api.opentyphoon.ai/v1',
+                    model='typhoon-instruct',
+                    api_key=openai_api_key)
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
